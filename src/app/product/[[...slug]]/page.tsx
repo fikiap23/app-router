8f7f454085907655711a006c50b425e040cd3e8f
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-11 00:28:27 +0700

    fetch api

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,17 +1,38 @@
 import React from 'react'
+import ProductCard from '../product-card'
 
-type DetailProductPageProps = {
+type ProductPageProps = {
   params: {
     slug: string[]
   }
 }
 
-export default function DetailProductPage(props: DetailProductPageProps) {
+async function getProduct() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  const data = await res.json()
+  return data
+}
+
+export default async function ProductPage(props: ProductPageProps) {
   const { params } = props
-  console.log(params)
+  const product = await getProduct()
+  console.log(product)
+  // console.log(params)
   return (
     <div>
-      <h1>{params.slug ? 'DetailProductPage' : 'ProductPage'}</h1>
+      <h1 className="text-3xl font-bold">
+        {params.slug ? 'DetailProductPage' : 'ProductPage'}
+      </h1>
+      {product.length > 0 && (
+        <div className="grid grid-cols-3 gap-4 place-items-center">
+          {product.map((product: any) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
       <h2>{params.slug}</h2>
     </div>
   )
