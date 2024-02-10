db6119b6c2863c9a8250641820eb87696badbcc9
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-10 23:30:27 +0700

    navigasi

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,79 @@
+import Link from 'next/link'
 import React from 'react'
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>
+  return (
+    <div className="h-screen  flex justify-center items-center">
+      <div className=" bg-blue-100 w-full h-full md:h-fit md:w-1/3  shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form className="space-y-6" action="#">
+          <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+            Sign up to our platform
+          </h3>
+          <div>
+            <label
+              htmlFor="fullname"
+              className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+            >
+              Fullname
+            </label>
+            <input
+              type="text"
+              name="fullname"
+              id="fullname"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="your fullname"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="name@company.com"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Sign up to your account
+          </button>
+          <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+            Already have an account?{' '}
+            <Link
+              href="/login"
+              className="text-blue-700 hover:underline dark:text-blue-500"
+            >
+              Login here
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default RegisterPage
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,22 +1,60 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import { usePathname, useRouter } from 'next/navigation'
 
 const Navbar = () => {
+  const pathname = usePathname()
+  const router = useRouter()
+  // console.log(pathname)
   return (
-    <nav className="flex bg-gray-800 py-2 px-5">
-      <h1 className="text-white">Navbar</h1>
-      <ul className="flex ml-auto text-white">
-        <Link href={'/'}>
-          <li className="mx-2 cursor-pointer">Home</li>{' '}
-        </Link>
-        <Link href={'/about'}>
-          <li className="mx-2 cursor-pointer">About</li>{' '}
-        </Link>
-        <Link href={'/about/profile'}>
-          {' '}
-          <li className="mx-2 cursor-pointer">Profile</li>{' '}
-        </Link>
-      </ul>
+    <nav className="flex bg-gray-800 py-2 px-5 justify-between items-center">
+      <div className="flex">
+        <h1 className="text-white">Navbar</h1>
+        <ul className="flex ml-5 text-white">
+          <Link href={'/'}>
+            <li
+              className={`mx-2 cursor-pointer ${
+                pathname === '/' && 'font-bold text-blue-200'
+              }`}
+            >
+              Home
+            </li>{' '}
+          </Link>
+          <Link href={'/about'}>
+            <li
+              className={`mx-2 cursor-pointer ${
+                pathname === '/about' && 'font-bold text-blue-200'
+              }`}
+            >
+              About
+            </li>{' '}
+          </Link>
+          <Link href={'/about/profile'}>
+            {' '}
+            <li
+              className={`mx-2 cursor-pointer ${
+                pathname === '/about/profile' && 'font-bold text-blue-200'
+              }`}
+            >
+              Profile
+            </li>{' '}
+          </Link>
+        </ul>
+      </div>
+      <div className="text-white flex gap-2">
+        <button
+          onClick={() => router.push('/login')}
+          className="cursor-pointer py-1 px-3 bg-blue-500 rounded-md"
+        >
+          Login
+        </button>
+        <button
+          onClick={() => router.push('/register')}
+          className="cursor-pointer py-1 px-3 bg-green-500 rounded-md"
+        >
+          Register
+        </button>
+      </div>
     </nav>
   )
 }
