261471285483a77a9881e495830ebafb9dcb6fe4
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-12 10:34:15 +0700

    add error handler

---
+++ src/app/product/[[...slug]]/error.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  useEffect(() => {
+    console.log(error)
+  }, [error])
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <p>{error.message}</p>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  )
+}
